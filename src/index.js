03f070f880c68c6c4ed2802db33f810570625542
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-09-30 12:12:11 +0900

    Chap08
    Event

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
-import Accommodate from './Chap07/Accommodate';
+
+import ConfirmButton from './Chap08/ConfirmButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Accommodate />
+    <ConfirmButton />
 );
