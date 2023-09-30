db68c94fed128c8c8682aec9df967984bbc25fcc
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-09-30 11:57:51 +0900

    Chap07
    Hook

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
-import NotificationList from './Chap06/NotificationList';
+import Accommodate from './Chap07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <NotificationList />
+    <Accommodate />
 );
